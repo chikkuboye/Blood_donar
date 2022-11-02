0ebecc3b8dcf74997445da8671c45642b24ff274
mahalakshmi <chikkuboye@gmail.com>
2022-11-02 19:57:48 +0530

    updated values

--- blood.py
+++ blood.py
@@ -28,16 +28,27 @@ while True:
         mycursor.execute(sql , data)
         mydb.commit()
     elif(choice==2):
-        print('view student')
+        print('view blood')
         sql = 'SELECT * FROM `blood_donater`'
         mycursor.execute(sql)
         result = mycursor.fetchall()
         for i in result:
             print(i)
     elif(choice==3):
-        print('search a student')
+        print('search a blood')
+        blood_group = input('enter the blood group to be searched: ')
+        
+        sql = "SELECT `id`, `Name`, `Phone_number`, `Address`, `total_blood_donated`, `blood_group` FROM `blood_donater` WHERE `blood_group`='"+blood_group+"'"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(result)
     elif(choice==4):
         print('update the student')
+        name = input('enter the name: ')
+        address = input('enter the address: ')
+        phone = input('enter the phone number: ')
+        total_blood_donated= input('enter the total blood donated: ')
+        #blood_group = input('enter the blood group: ')
     elif(choice==5):
         print('delete the student')
     elif(choice==6):
